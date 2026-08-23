8317dd197db340faa08d8f2b53b76e4ece9170c9
Ranslmware <ranslmware40@gmail.com>
2026-08-23 15:41:00 -0300

    feat: adiciona métodos restantes ao product respository

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"go-api/model"
 )
@@ -19,14 +20,11 @@ func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
 
 	rows, err := pr.conn.Query(query)
 
-	if rows.Err() != nil {
-		return []model.Product{}, nil
+	if err != nil {
+		return []model.Product{}, fmt.Errorf("Error retrieving the product: %w", err)
 	}
 
-	if err != nil {
-		fmt.Println(err)
-		return []model.Product{}, nil
-	}
+	defer rows.Close()
 
 	var productList []model.Product
 	var productObj model.Product
@@ -38,16 +36,97 @@ func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
 			&productObj.Price)
 
 		if err != nil {
-			fmt.Println(err)
-			return []model.Product{}, nil
+			return []model.Product{}, err
 		}
 
 		productList = append(productList, productObj)
 
 	}
 
-	rows.Close()
-
 	return productList, nil
+
+}
+
+func (pr *ProductRepository) CreateProduct(p model.Product) (int, error) {
+
+	var id int
+
+	query := `INSERT INTO product (product_name, price)
+	VALUES ($1, $2)
+	RETURNING id`
+
+	err := pr.conn.QueryRow(query, p.Name, p.Price).Scan(&id)
+
+	if err != nil {
+		return 0, fmt.Errorf("Error to create product: %w", err)
+	}
+
+	return id, nil
+
+}
+
+func (pr *ProductRepository) GetProductByID(id_product int) (*model.Product, error) {
+
+	query := `SELECT id, product_name, price
+	FROM product
+	WHERE id = $1`
+
+	var product model.Product
+
+	err := pr.conn.QueryRow(query, id_product).Scan(&product.ID, &product.Name, &product.Price)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("Error: %w", err)
+		}
+		return nil, fmt.Errorf("Error trying to find the product: %w", err)
+	}
+
+	return &product, nil
+
+}
+
+func (pr *ProductRepository) DeleteById(id_product int) error {
+
+	query := `DELETE FROM product WHERE id = $1`
+
+	row, err := pr.conn.Exec(query, id_product)
+
+	if err != nil {
+		return err
+	}
+
+	affected, _ := row.RowsAffected()
+
+	if affected == 0 {
+		return fmt.Errorf("Product not found")
+	}
+	return nil
+
+}
+
+func (pr *ProductRepository) UpdateById(id_product int, product model.Product) error {
+
+	query := `UPDATE product 
+	SET product_name = $1, price = $2
+	WHERE id = $3`
+
+	result, err := pr.conn.Exec(query, product.Name, product.Price, id_product)
+
+	if err != nil {
+		return fmt.Errorf("Error updating the product: %w", err)
+	}
+
+	affected, err := result.RowsAffected()
+
+	if err != nil {
+		return fmt.Errorf("Error checking for updates: %w", err)
+	}
+
+	if affected == 0 {
+		return fmt.Errorf("Product not found")
+	}
+
+	return nil
 
 }

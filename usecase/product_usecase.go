3e2eb9d3385dae1f322ab858864e901606fdd8de
Ranslmware <ranslmware40@gmail.com>
2026-08-23 15:41:40 -0300

    feat: adiciona métodos restantes ao product usecase

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -19,3 +19,52 @@ func (p *ProductUseCase) GetProducts() ([]model.Product, error) {
 	return p.repo.GetProducts()
 
 }
+
+func (pu *ProductUseCase) CreateProduct(product model.Product) (model.Product, error) {
+
+	productID, err := pu.repo.CreateProduct(product)
+
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productID
+
+	return product, nil
+
+}
+
+func (pr *ProductUseCase) GetProductByID(product_id int) (*model.Product, error) {
+
+	product, err := pr.repo.GetProductByID(product_id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+
+}
+
+func (pr *ProductUseCase) DeleteById(product_id int) error {
+
+	err := pr.repo.DeleteById(product_id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pr *ProductUseCase) UpdateById(product_id int, product model.Product) error {
+
+	err := pr.repo.UpdateById(product_id, product)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
